1b60d742538ac28b2715a13a341c1874c2e9ec37
AnthonyAguilarP <anthonyaguilarparrales2005@gmail.com>
2026-01-04 18:08:04 -0600

    make a refactoring to the project, for the books

---
+++ src/registry/AmericanSpeakOutStarter/6/BooksPage6.js
@@ -0,0 +1,5 @@
+import A from '../../../components/AmericanSpeakOut/AmericanSpeackOutStarter/6/Section1/A.astro';
+
+export const BooksPage_6 = {
+    A
+}
---
+++ src/registry/AudiosRouter.js
@@ -0,0 +1,9 @@
+import SL1 from '../assets/Audio/AmericanSpeakOut/Starter/SL1.opus';
+import SL2 from '../assets/Audio/AmericanSpeakOut/Starter/SL2.opus';
+import SL3 from '../assets/Audio/AmericanSpeakOut/Starter/SL3.opus';
+
+export const AudiosComponent_6 = {
+    SL1,
+    SL2,
+    SL3
+}
